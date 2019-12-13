036a5830f3c56cd4a7ed718ea891ab421f5e4c97
DIETER BREHM <inkering@gmail.com>
2019-12-13 15:57:12 -0500

    link to subpages

--- .vuepress/theme/components/Nav.vue
+++ .vuepress/theme/components/Nav.vue
@@ -4,11 +4,11 @@
 				<div class="elements">
 						<router-link to="/"><h1 class="nav-link" v-on:click="home">Strawstairies</h1></router-link>
 						<h1>//</h1>
-						<h1 class="nav-link" v-on:click="Sprint">Sprint</h1>
+						<router-link to="/sprints"></a><h1 class="nav-link" v-on:click="Sprint">Sprint</h1></router-link>
 						<h1>//</h1>
-						<h1 class="nav-link" v-on:click="projects">Budget</h1>
+						<router-link to="/budget"></a><h1 class="nav-link" v-on:click="projects">Budget</h1></router-link>
 						<h1>//</h1>
-						<h1 class="nav-link" v-on:click="blog">Blog</h1>
+						<router-link to="/"></a><h1 class="nav-link" v-on:click="blog">Blog</h1></router-link>
 				</div>
 		</div>
   </header>
