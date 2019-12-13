7c3e77f914861bc1c99e2ff6d0d8bfe47b63b850
DIETER BREHM <inkering@gmail.com>
2019-12-13 16:36:44 -0500

    create blog again

---
+++ .vuepress/theme/layouts/BlogPage.vue
@@ -0,0 +1,71 @@
+<template>
+	<div>
+		<img class="rotate" src="circle.png" alt="">
+		<div class="main">
+			<header class="header">
+
+
+				<img class="home-logo" src="strawberry.png" alt="">
+				<h1 id="about">The<br>Strawstairies</h1>
+				<Nav />
+
+			<img class="home-animation" src="animation.gif" alt="">
+
+			</header>
+			<h1 id="blog">Blog</h1>
+			<Blog />
+			<Footer />
+		</div>
+	</div>
+</template>
+
+<script>
+	import 'normalize.css'
+	import Vue from 'vue'
+	// internal components
+	import Nav from "@theme/components/Nav";
+	import Footer from "@theme/components/Footer";
+	import ProjectList from "@theme/components/ProjectList";
+	import Blog from "@theme/components/Blog";
+	import { library } from '@fortawesome/fontawesome-svg-core'
+	import { faPaperPlane, faCaretRight} from '@fortawesome/free-solid-svg-icons'
+	import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+	
+	library.add(faCaretRight)
+	Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+	// moment js for transforming, creating, and parsing dates
+	import moment from "moment"
+
+	// poppins font
+	import 'typeface-poppins'
+
+	export default {
+		components: {
+			Nav,
+			Footer,
+			ProjectList,
+			Blog
+		},
+		name: "Layout",
+		methods: {
+			formatDate(date) {
+				return moment(date).format("MMM Do YYYY");
+			}
+		},
+		computed: {
+			mechanics: function () {
+				return this.$site.pages
+					.filter(x => x.path.startsWith("/blog/"))
+					.sort(
+							(a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date)
+					);
+			},
+		}
+	};
+</script>
+
+<style>
+	/* @import url('https://fonts.googleapis.com/css?family=Goudy+Bookletter+1911|Lato&display=swap'); */
+	@import '../styles/theme.css';
+</style>
---
+++ BlogPage.md
@@ -0,0 +1,6 @@
+---
+title: "Blog"
+layout: BlogPage
+date: 2019-11-14
+emoji: 📯
+---
