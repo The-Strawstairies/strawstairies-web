716f18a8df0452a8911787c1b6c9148ba51476f7
DIETER BREHM <inkering@gmail.com>
2019-12-13 13:26:14 -0500

    add color blocks

--- .vuepress/theme/layouts/Layout.vue
+++ .vuepress/theme/layouts/Layout.vue
@@ -9,18 +9,27 @@
 			<img src="/animation.gif" alt="">
 
 
-			<p class="aboutText">We're a Principles of Engineering team at Olin College of Engineering currently making
+			<div class="colorblock mechanics">
+				<p>hi</p>
+			</div>
+			<div class="colorblock electronics">
+				<p>hi</p>
+			</div>
+			<div class="colorblock software">
+				<p>hi</p>
+			</div>
+			<!-- <p class="colorblock aboutText">We're a Principles of Engineering team at Olin College of Engineering currently making
 				a stair climbing robot which is able to protect snacks and other objects during movement. It features
-				cute wheel-legs (whegs), active snack balancing technology, and wireless control!</p>
-			<img class="logo" src="/robot.png" alt="" />
-			<h1 id="projects">Features</h1>
+				cute wheel-legs (whegs), active snack balancing technology, and wireless control!</p> -->
+			<!-- <img class="logo" src="/robot.png" alt="" /> -->
+			<!-- <h1 id="projects">Features</h1>
 			<ul>
 				<li>It climbs stairs!</li>
 				<li>It holds snacks and other objects!</li>
 				<li>wirelessly controllable!</li>
 			</ul>
 			<h1 id="blog">Blog</h1>
-			<Blog />
+			<Blog /> -->
 			<Footer />
 		</div>
 	</div>
